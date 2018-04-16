a72431c8fe51508c8810c7d8bcea75a61d252cde
Sébastien Chopin <seb@chopin.io>
2018-04-16 16:16:02 +0200

    fix: pkg undefined

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Nuxt.js demo with Moltin API for e-commerce' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
