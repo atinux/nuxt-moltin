139578ebacc9e266217723de1f7d2c8256daea47
Alex <alex@orion.sh>
2018-04-16 15:38:50 +0200

    login/register

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,73 @@
 <template>
-  <pre>Register</pre>
+  <div class="Login">
+    <h1 class="Login__Title">Log in to your account</h1>
+    <form action="/" @submit.prevent="login">
+      <label class="Login__Form__Label" for="email">Email</label>
+      <input v-model="form.email" class="Login__Form__Input" id="email" type="email">
+      <label class="Login__Form__Label" for="pwd">Password</label>
+      <input v-model="form.password" class="Login__Form__Input" id="pwd" type="password">
+      <button class="Login__Form__Button" type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.form)
+    }
+  }
 }
 </script>
+
+<style>
+.Login {
+
+}
+.Login__Title {
+  font-size: 24px;
+  font-weight: 400;
+  padding: 20px 0;
+}
+.Login__Form {
+
+}
+.Login__Form__Label {
+  display: block;
+  color: #555;
+  font-size: 14px;
+  padding-bottom: 7px;
+}
+.Login__Form__Input {
+  width: 100%;
+  display: block;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  height: 36px;
+  padding: 0 5px;
+}
+.Login__Form__Input:focus {
+  outline: none;
+  border-color: #E06A2C;
+}
+.Login__Form__Button {
+  color: #fff;
+  background-color: #F2711C;
+  border-radius: 4px;
+  padding: 10px 20px;
+}
+.Login__Form__Button:hover {
+  background-color: #E06A2C;
+}
+</style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,9 +1,76 @@
 <template>
-  <pre>Register</pre>
+  <div class="Register">
+    <h1 class="Register__Title">Create an account</h1>
+    <form action="/" @submit.prevent="register">
+      <label class="Register__Form__Label" for="name">Name</label>
+      <input v-model="form.name" class="Register__Form__Input" id="name" type="text">
+      <label class="Register__Form__Label" for="email">Email</label>
+      <input v-model="form.email" class="Register__Form__Input" id="email" type="email">
+      <label class="Register__Form__Label" for="pwd">Password</label>
+      <input v-model="form.password" class="Register__Form__Input" id="pwd" type="password">
+      <button class="Register__Form__Button" type="submit">Register</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    register () {
+      console.log(this.form)
+    }
+  }
 }
 </script>
+
+<style>
+.Register {
+
+}
+.Register__Title {
+  font-size: 24px;
+  font-weight: 400;
+  padding: 20px 0;
+}
+.Register__Form {
+
+}
+.Register__Form__Label {
+  display: block;
+  color: #555;
+  font-size: 14px;
+  padding-bottom: 7px;
+}
+.Register__Form__Input {
+  width: 100%;
+  display: block;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  height: 36px;
+  padding: 0 5px;
+}
+.Register__Form__Input:focus {
+  outline: none;
+  border-color: #E06A2C;
+}
+.Register__Form__Button {
+  color: #fff;
+  background-color: #F2711C;
+  border-radius: 4px;
+  padding: 10px 20px;
+}
+.Register__Form__Button:hover {
+  background-color: #E06A2C;
+}
+</style>
